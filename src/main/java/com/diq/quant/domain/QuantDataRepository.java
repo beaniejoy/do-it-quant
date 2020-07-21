e7e3db7e503f9954b05a5235175c91d88daa0adb
hanbinleejoy <lhb1025@gmail.com>
2020-07-21 17:16:45 +0900

    200721 Query mariadb에 맞게 수정

--- src/main/java/com/diq/quant/domain/QuantDataRepository.java
+++ src/main/java/com/diq/quant/domain/QuantDataRepository.java
@@ -10,40 +10,39 @@ public interface QuantDataRepository extends JpaRepository<QuantData, Long>{
 	@Query(value = "TRUNCATE TABLE quant_data", nativeQuery = true)
 	Object truncateTable();
 
-	@Query(value = "WITH table_rank_pbr " + 
-			"AS (SELECT id, cmp_name, pbr, " + 
+	@Query(value = "WITH table_rank_pbr AS ((SELECT id, cmp_name, pbr, " + 
 			"RANK() OVER(ORDER BY pbr) rank_pbr " + 
-			"FROM QUANT_DATA " + 
-			"WHERE pbr > 0 " + 
-			"UNION " + 
-			"SELECT id, cmp_name, pbr, " + 
-			"(RANK() OVER(ORDER BY pbr DESC) + SELECT COUNT(id) " + 
-			"FROM QUANT_DATA " + 
-			"WHERE pbr> 0) rank_pbr " + 
-			"FROM QUANT_DATA " + 
-			"WHERE pbr <= 0 or pbr is null) " + 
-			",table_rank_per  " + 
-			"AS (SELECT id, cmp_name, per, " + 
+			"FROM quant_data " + 
+			"WHERE pbr > 0) " + 
+			"UNION  " + 
+			"(SELECT id, cmp_name, pbr, " + 
+			"(RANK() OVER(ORDER BY pbr DESC) + (SELECT COUNT(id) " + 
+			"FROM quant_data " + 
+			"WHERE pbr> 0)) rank_pbr " + 
+			"FROM quant_data " + 
+			"WHERE pbr <= 0 or pbr is null)) " + 
+			",table_rank_per " + 
+			"AS ((SELECT id, cmp_name, per, " + 
 			"RANK() OVER(ORDER BY per) rank_per " + 
-			"FROM QUANT_DATA " + 
-			"WHERE per > 1 " + 
+			"FROM quant_data " + 
+			"WHERE per > 1) " + 
 			"UNION " + 
-			"SELECT id, cmp_name, per, " + 
-			"(RANK() OVER(ORDER BY per DESC) + SELECT COUNT(id) " + 
-			"FROM QUANT_DATA " + 
-			"WHERE per> 1) rank_per " + 
-			"FROM QUANT_DATA " + 
-			"WHERE per <= 1 OR per is null) " + 
+			"(SELECT id, cmp_name, per, " + 
+			"(RANK() OVER(ORDER BY per DESC) + (SELECT COUNT(id) " + 
+			"FROM quant_data " + 
+			"WHERE per> 1)) rank_per " + 
+			"FROM quant_data " + 
+			"WHERE per <= 1 OR per is null)) " + 
 			"SELECT " + 
 			"a.id, a.cmp_name, a.code, " + 
-			"RANK() OVER(ORDER BY NVL(a.DEBT_RATIO, 99999)) rank_debt_ratio, " + 
-			"RANK() OVER(ORDER BY a.RESERVE_RATIO DESC) rank_reserve_ratio, " + 
-			"RANK() OVER(ORDER BY a.OPERATING_PROFIT_RATIO DESC) rank_oper, " + 
-			"RANK() OVER(ORDER BY a.ROA DESC) rank_roa, " + 
-			"RANK() OVER(ORDER BY a.ROE DESC) rank_roe, " + 
-			"b.RANK_PBR, " + 
-			"c.RANK_PER, " + 
-			"FROM QUANT_DATA a, table_rank_pbr b, table_rank_per c " + 
-			"WHERE a.id = b.id AND a.id=c.id; ", nativeQuery = true)
+			"RANK() OVER(ORDER BY NVL(a.debt_ratio, 99999)) rank_debt_ratio, " + 
+			"RANK() OVER(ORDER BY a.reserve_ratio DESC) rank_reserve_ratio, " + 
+			"RANK() OVER(ORDER BY a.operating_profit_ratio DESC) rank_oper, " + 
+			"RANK() OVER(ORDER BY a.roa DESC) rank_roa, " + 
+			"RANK() OVER(ORDER BY a.roe DESC) rank_roe, " + 
+			"b.rank_pbr, " + 
+			"c.rank_per " + 
+			"FROM quant_data a, table_rank_pbr b, table_rank_per c " + 
+			"WHERE a.id=b.id AND a.id=c.id; ", nativeQuery = true)
 	List<Object> findAllByRank();
 }
